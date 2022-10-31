c5c51b8c797af264934c56110931cd9d46571c32
Jyothi-Prakash18 <116968893+Jyothi-Prakash18@users.noreply.github.com>
2022-10-31 11:10:14 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,17 @@
+import React from 'react'
+const FunctionalCount = () => {
+    return(
+    <div>
+    <div class ="slide">
+    <h1>ABOUT UI/UX</h1>
+    <h4>User Interface design is what makes a design engaging and delightful for consumers. This course helps you understand the fundamentals of aesthetics and visual design, typography, color, layouts, and visual hierarchy to create beautiful user interfaces.</h4>
+    </div>
+    <button><a href="https://www.simplilearn.com/ui-ux-design-masters-program-certification-course?utm_source=google&utm_medium=cpc&utm_term=&utm_content=17745083515-139323871856-610716893927&utm_device=c&utm_campaign=Search-DigitalBusinessCluster-BusinessLeadership-UI-UX-DSA-IN-Main-AllDevice-adgroup-UI-UX-DSA&gclid=Cj0KCQjwnvOaBhDTARIsAJf8eVP_IaWDnZwSA8mdkTEy-llukk2Mrrms-QmUfvPU9Avyt9sR_Butup0aAuRcEALw_wcB">More info</a></button>
+    </div>
+    );
+}
+ 
+export default FunctionalCount;
+
+
+
